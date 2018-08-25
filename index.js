b8a88f234faf17d08057f109bb869dc1b5819f6b
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-25 23:51:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let neighborhoodId = 0;
+let customerId = 0;
+let mealId = 0;
+
+class Neighborhood {
+  constructor(name){
+    this.id = ++neighborhoodId;
+    this.name = name;
+    store.neighborhoods.push(this);
+  }
+}
+
+class Customer {
+  constructor(name, neighborhood){
+    this.id = ++neighborhoodId;
+    this.name = name;
+    this.neighborhood = neighborhood;
+    store.customers.push(this);
+  }
+}
+
+class Meal {
+  constructor(title, price){
+    this.id = ++mealId;
+    this.title = title;
+    this.price = price;
+    store.meals.push(this);
+  }
+}
