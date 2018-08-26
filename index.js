102c1fd77c6ded4cdc6901ece27227fd83506a9b
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-26 00:24:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ class Customer {
     })
   }
   meals(){
-    return this.deliveries().map(delivery => {
+    return this.deliveries().filter(delivery => {
       return delivery.meal();
     })
   }
