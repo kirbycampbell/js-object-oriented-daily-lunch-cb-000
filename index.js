bc298a7f01de0b502f69cd5778db4ba951ef2a11
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-26 00:46:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ class Neighborhood {
     });
   }
   meals(){
-    return this.deliveries().map(delivery => {
+    return this.deliveries().filter(delivery => {
       return delivery.neighborhood();
     })
   }
