e7faf01c26d81b6ad3fd28e16c2faf19bc7bbbe2
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-26 00:41:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ class Customer {
     })
   }
   totalSpent(){
-    return this.meals().reduce((total, meal) => (total += meal.price), 0);
+    return this.meals().reduce((total, meal) => (total += meal.price), 1);
   }
 }
 
